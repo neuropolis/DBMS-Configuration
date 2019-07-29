a56bd7af460dd0c0c558d690a1a412328dafebfe
zhangyiloong <zhangyiloong@gmail.com>
2019-07-29 10:54:37 +0800

    Add fixed files

---
+++ AutoTuner/environment/configs.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+description: MySQL Database Configurations
+"""
+
+instance_config = {'mysql1' : 
+			{
+			'host' : '127.0.0.1',
+			'user' : 'root',
+			'passwd' :'',
+			'password' :'',
+			'port' : 3306,
+			'max_mem' : 10,
+			'max_disk' : 20
+			}
+		}
